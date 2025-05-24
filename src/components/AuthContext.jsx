8aae7c6e87373188b345978086a2901a715ae057
Srikanth Kamalla <srikanthkamalla2919@gmail.com>
2025-05-24 13:13:28 +0530

    p4

--- src/components/AuthContext.jsx
+++ src/components/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from "react";
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [isHuman, setIsHuman] = useState(true);
+  const [isHuman, setIsHuman] = useState(false);
   return (
     <AuthContext.Provider value={{ isHuman, setIsHuman }}>
       {children}
